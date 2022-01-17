1d1998aea9980abc654476aa4ca6770a39d315d3
sulaymonss <bwstormsss@gmail.com>
2022-01-17 18:12:04 +0500

    changed

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use('/assets', express.static(path.resolve(__dirname, 'public')))
 app.post('/contact', (req, res) => {
     const { name, email, message } = req.body
     nodeMailer(email)
-    const oldData = JSON.parse(fs.readFileSync(path.resolve(__dirname, './model', 'data.json')))
-    oldData.push({
-        id: oldData.length + 1 ,name, email, message
-    })
-    fs.writeFileSync(path.resolve(__dirname, './model', 'data.json'), JSON.stringify(oldData, null, 4))
+    // const oldData = JSON.parse(fs.readFileSync(path.resolve(__dirname, './model', 'data.json')))
+    // oldData.push({
+    //     id: oldData.length + 1 ,name, email, message
+    // })
+    // fs.writeFileSync(path.resolve(__dirname, './model', 'data.json'), JSON.stringify(oldData, null, 4))
     
     res.send('Successfully sent')
 })
